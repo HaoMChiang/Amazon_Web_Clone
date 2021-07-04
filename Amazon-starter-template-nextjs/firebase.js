64b4fa1b22272fc4447007c11bad2048a4e9a8d7
Hao Chiang <hmc@Haos-MacBook-Pro.local>
2021-07-03 23:45:20 -0700

    updated checkout and authentication

---
+++ Amazon-starter-template-nextjs/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvMJs_Dl2fmhUh_88uoOV2e0GoU0gVhL0",
+  authDomain: "web-clone-f3f2a.firebaseapp.com",
+  projectId: "web-clone-f3f2a",
+  storageBucket: "web-clone-f3f2a.appspot.com",
+  messagingSenderId: "73675084433",
+  appId: "1:73675084433:web:ba17c518a503d1238b6530",
+};
